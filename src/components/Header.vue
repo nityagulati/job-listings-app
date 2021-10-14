f5c9c18f6d5ec47ccdd9f1b2c4a6ce56ec78b4e7
Nitya Gulati <46540503+nityagulati@users.noreply.github.com>
2021-10-14 03:03:12 -0400

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header"></div>
+</template>
+
+<script>
+export default {
+    name: 'header',
+}
+</script>
+
+<style lang="scss">
+.header {
+    height: 156px;
+    background-color: $dark-cyan;
+    background-image: url('../assets/images/bg-header-desktop.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}  
+</style>
