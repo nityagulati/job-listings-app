86267cb0740cafc53ce0075a204412da9081a731
Nitya Gulati <46540503+nityagulati@users.noreply.github.com>
2021-12-09 19:16:08 -0500

    add job listing component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <Header />
+  <div class="main">
+    <JobListing />
+  </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import JobListing from './components/JobListing.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    JobListing
   }
 }
 </script>
---
+++ src/components/JobListing.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="job-card" v-for="listing in listings" :key="listing.id">
+        <img class="job-card__logo" :src="getLogo(listing.logo)" alt="company logo">
+        <div class="job-card__info">
+            <div class="job-card__company">
+                <span>{{listing.company}}</span>
+                <span class="callout callout--new" v-if="listing.new">NEW!</span>
+                <span class="callout callout--feat" v-if="listing.featured">FEATURED</span>
+            </div>
+            <p class="job-card__position">{{listing.position}}</p>
+            <div class="job-card__details">
+                <span>{{listing.postedAt}}</span>
+                <span class="dot">.</span>
+                <span>{{listing.contract}}</span>
+                <span class="dot">.</span>
+                <span>{{listing.location}}</span>
+            </div>
+        </div>
+        <div class="job-card__tags">
+            <span>{{listing.role}}</span>
+            <span>{{listing.level}}</span>
+            <span v-for="(language, index) in listing.languages" :key="index">{{language}}</span>
+            <span v-for="(tool, index) in listing.tools" :key="index">{{tool}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import json from "@/assets/files/listings.json"
+
+export default {
+    name: 'job-listing',   
+    data() {
+        return {
+            listings: json.listings,
+        }
+    },
+    methods: {
+        getLogo: function(logo) {
+            return require('@/assets/images/' + logo)
+        }
+    },
+}
+</script>
